f91f8054e6cb2b7e7a2e38c7346a10d1a97b1838
ShivaanshK <shivaanshkapoor02@gmail.com>
2018-07-29 12:23:06 -0700

    Update Insureum.sol

--- contracts/Insureum.sol
+++ contracts/Insureum.sol
@@ -2,31 +2,31 @@ pragma solidity ^0.4.17;
 
 contract Insureum {
 
-    mapping (address => uint) private insurancePlan;
+   mapping (address => uint) private insurancePlan;
+     
 
-    address public hospital;
+   function Insureum() public {
+      
+   }
+   function planOneTwoThree() public payable returns (uint256) {
+       // Here we are making sure that there isn’t an overflow issue
+       require((insurancePlan[msg.sender] + msg.value) >= insurancePlan[msg.sender]);
 
-    constructor () public {
-        hospital = msg.sender;
+       insurancePlan[msg.sender] += msg.value;
+
+       return insurancePlan[msg.sender];
+       
+   }
+
+   function claim(address , uint256 withdrawAmount) public {
+
+   }
+
+   function getBalance() public constant returns (uint256) {
+        return address(this).balance;
     }
 
-    function PayInPremium() public payable returns (uint256) {
-        // Here we are making sure that there isn't an overflow issue
-        require((insurancePlan[msg.sender] + msg.value) >= insurancePlan[msg.sender]);
-        insurancePlan[msg.sender] += msg.value;
-        return insurancePlan[msg.sender];
-    }
-
-    function claim(uint withdrawAmount) public {
-        require(msg.sender == hospital);
-        msg.sender.transfer(withdrawAmount);
-    }
-
-    function getBalanceOfContract() public constant returns (uint256) {
-         return address(this).balance;
-     }
-
-    function getBalanceOfUser() public constant returns (uint256) {
-        return insurancePlan[msg.sender];
-    }
+   function getBalanceOfUser() public constant returns (uint256) {
+       return insurancePlan[msg.sender];
+   }
 }
