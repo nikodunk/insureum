f2bf070546f72594d41cc5f2abe958e76dcb1292
Nikolai <n.dunkel@gmail.com>
2018-07-29 09:42:25 -0700

    corrected build paths

--- truffle.js
+++ truffle.js
@@ -8,5 +8,12 @@ module.exports = {
       port: 7545,
       network_id: '*' // Match any network id
     }
-  }
+  },
+  rinkeby: {
+      host: "localhost", // Connect to geth on the specified
+      port: 8545,
+      from: "0x2df219a1ae3845ab32a041499db27d4bfdb43876", // default address to use for any transaction Truffle makes during migrations
+      network_id: 4,
+      gas: 4612388 // Gas limit used for deploys
+    }
 }
