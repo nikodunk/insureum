6f1d2fb91f332316df10fc9a52197ff09b3e5478
Nicholas Dunkel <nicholasdunkel@NicholasDunkel.local>
2018-07-31 11:48:10 -0700

    working with libraries

--- truffle.js
+++ truffle.js
@@ -1,18 +1,9 @@
 const HDWalletProvider = require("truffle-hdwallet-provider");
-
-// DELETE ME!!!!// DELETE ME!!!!// DELETE ME!!!!// DELETE ME!!!!
-// DELETE ME!!!!// DELETE ME!!!!// DELETE ME!!!!// DELETE ME!!!!
-// DELETE ME!!!!// DELETE ME!!!!// DELETE ME!!!!// DELETE ME!!!!
-var mnemonic = "virtual armed pumpkin stadium crucial sign shop nuclear purpose shield cave tomato"; // DELETE ME!!!!
-// DELETE ME!!!!// DELETE ME!!!!// DELETE ME!!!!// DELETE ME!!!!
-// DELETE ME!!!!// DELETE ME!!!!// DELETE ME!!!!// DELETE ME!!!!
-// DELETE ME!!!!// DELETE ME!!!!// DELETE ME!!!!// DELETE ME!!!!
-
-
-
-
+var mnemonic = "virtual armed pumpkin stadium crucial sign shop nuclear purpose shield cave tomato"; 
 // Allows us to use ES6 in our migrations and tests.
 require('babel-register')
+
+
 
 module.exports = {
   networks: {
@@ -22,8 +13,8 @@ module.exports = {
       network_id: '*' // Match any network id
     },
     rinkeby: {
-      provider: new HDWalletProvider(mnemonic, "https://rinkeby.infura.io/v3/c7ccca1b03e943ada5f6b22ed5cc3cc0"),
-      network_id: "4",  
+      provider: new HDWalletProvider(mnemonic, "https://rinkeby.infura.io/c7ccca1b03e943ada5f6b22ed5cc3cc0"),
+      network_id: '*',  
       gas: 4500000,
       gasPrice: 25000000000
     }
